a869d5e2660eff8be45e40715795abbfde90b0fb
Syntaxsh <ashleyotchengco@gmail.com>
2023-04-23 23:54:31 +0800

    add login and registration

---
+++ src/Components/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { HashLink as Link } from 'react-router-hash-link'
+
+import '../Style/Login.css'
+
+const Login = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h2>LOGIN</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div className="input-div">
+                    <label htmlFor="username">Username:</label>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="Username" id="username" name="username" required />
+                </div>
+                <div className="input-div">
+                    <label htmlFor="password">Password:</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" name="password" required />
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+            <span className="link-button">Don't have an account?<Link className="link" to="/register"> Register here.</Link></span>
+        </div>
+    )
+}
+export default Login
---
+++ src/Components/Register.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { HashLink as Link } from 'react-router-hash-link';
+
+import '../Style/Register.css'
+
+const Register = (props) => {
+    const { position, setPosition } = useState('');
+    const { password, setPassword } = useState('');
+    const { name, setName } = useState('');
+    const [gender, setGender] = useState('');
+    const [username, setUsername] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(position);
+    }
+
+    return (
+        <div className="auth-form-container1">
+            <h2>REGISTER</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
+
+                <div className="input-div">
+                    <label>Full Name:</label>
+                    <input value={name} name="name" id="name" placeholder="Full Name" />
+                </div>
+
+                <div className="rdbtn">
+                    <div>
+                        <label htmlFor="gender">Gender:</label>
+                    </div>
+                    <div className="rdbtnrow">
+                        <div>
+                            <label htmlFor="male">Male</label>
+                            <input type="radio" name="gender" value="male" checked={gender === 'male'} onChange={(e) => setGender(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="female">Female</label>
+                            <input type="radio" name="gender" value="female" checked={gender === 'female'} onChange={(e) => setGender(e.target.value)} />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="input-div">
+                    <label>Position:</label>
+                    <select value={position} onChange={(e) => setPosition(e.target.value)} id="position" name="position">
+                        <option value="">Select Position</option>
+                        <option value="web">Web Developer</option>
+                        <option value="ui">UI/UX Designer</option>
+                        <option value="qa">QA Tester</option>
+                    </select>
+                </div>
+
+                <div className="input-div">
+                    <label htmlFor="username">Username:</label>
+                    <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="Username" name="username" />
+                </div>
+
+                <div className="input-div">
+                    <label htmlFor="password">Password:</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" name="password" />
+                </div>
+
+                <div className="input-div">
+                    <label htmlFor="password">Confirm Password:</label>
+                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" name="password" />
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+            <span className="link-button1">Already have an account?<Link className="link" to="/login"> Login here.</Link></span>
+        </div>
+    )
+}
+export default Register
